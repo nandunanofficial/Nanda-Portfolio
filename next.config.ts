4ad3170f2c0ee5a3309381f7c80d053975fe29b2
nandunanofficial <nandunan264@gmail.com>
2026-02-12 01:57:48 +0530

    Remove GitHub Pages config, deploy to Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: false,
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  assetPrefix: '/Nanda-Portfolio',
-  basePath: '/Nanda-Portfolio'
 };
 
 export default nextConfig;
